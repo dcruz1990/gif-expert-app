bf400d48cd8f9aae5acb22b3d1f57f13b8181e60
dcruz1990 <dcruzbv1990@gmail.com>
2021-11-03 15:10:42 -0400

    Back to JS

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,12 +1,24 @@
-import { shallow } from "enzyme"
-import { AddCategory } from "../../components/AddCategory"
+import { shallow } from "enzyme";
+import { AddCategory } from "../../components/AddCategory";
 
-describe('Pruebas en AddCategory', () => {
+describe("Pruebas en AddCategory", () => {
+  const wrapper = shallow(<AddCategory setCategories={AddCategory} />);
 
-    const setCategories = () => {}
-    test('should show correctly', () => {
-        const wrapper = shallow(<AddCategory setCategories={AddCategory}/>)
-        expect(wrapper).toMatchSnapshot()
-    })
-    
-})
+  const setCategories = () => {};
+
+  test("should show correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should change text box", () => {
+    const input = wrapper.find("input");
+
+    const value = "Hola mundo";
+
+    input.simulate("change", {
+      target: {
+        value,
+      },
+    });
+  });
+});
